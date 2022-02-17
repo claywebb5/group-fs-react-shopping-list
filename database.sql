1087dc688992fea3809ca0a76c29530faf4fc7a9
Andy Swanson <swan1625@icloud.com>
2022-02-17 14:12:17 -0600

    database data added

--- database.sql
+++ database.sql
@@ -1,2 +1,17 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+--DATABASE name: react-shopping-list ----
+
+
+
+CREATE TABLE "list" (
+	"id" SERIAL PRIMARY KEY,
+	"item" varchar(80),
+	"quantity" DECIMAL(5,2),
+	"unit" varchar(20)
+);
+
+
+INSERT into "list" ( "item", "quantity", "unit")
+VALUES ( 'bananas', 3.3, 'bunches'); 
